83394698c54ce9d001274c664f6ce7e29a564208
Tobias Küchel <t.kuechel@humboldt-ka.de>
2020-04-02 00:54:55 +0200

    add funktionscheck, logout ebenfalls in async - loop

--- linuxmuster-enroll-classes.py
+++ linuxmuster-enroll-classes.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+
+#import sys
 import json
 import hmac
 import hashlib
@@ -6,15 +8,40 @@ import requests
 import asyncio
 from nio import * 
 import configparser
+import subprocess
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 bot_id = config['bot']['id']
 bot_passwd = config['bot']['passwd']
-homeserver = config['homeserver']['url'] 
+homeserver = config['homeserver']['url']
+id_domain = bot_id.split(":")[1]
 shared_secret = config['impersonation']['secret']  ## needs to be byte-data
 client = AsyncClient(homeserver, bot_id)
+
+
+def check_functionality():
+
+    ## Wir brauchen sophomorix um Gruppen herauszufinden und aufzulösen
+    try:
+        completedprocess = subprocess.run(["sophomorix-class", "-h"])
+    except OSError as e:
+        print("Execution failed:", e, file=sys.stderr)
+        logout_response = loop.run_until_complete(logout())
+        raise SystemExit
+
+    ## Wir brauchen auch Zugang zur Circles API, wenn wir die Kreise
+    ## auflösen wollen
+#    try:
+#        completedprocess = subprocess.run("???")
+#    except OSError as e:
+#        print("Execution failed:", e, file=sys.stderr)
+#        logout_response = loop.run_until_complete(logout())
+#        raise SystemExit
+
+
+
 
 
 def get_impersonation_token(user_id, homeserver, shared_secret):
@@ -36,7 +63,6 @@ def get_impersonation_token(user_id, homeserver, shared_secret):
 
 async def call_on_invites(room, event):
 
-
     # also possible InviteAliasEvent
     if (not isinstance(event, InviteMemberEvent)):
         print("This is not an InviteMemberEvent!", event)
@@ -46,12 +72,13 @@ async def call_on_invites(room, event):
     if ( event.membership != "invite" ):
         print("This is not an invitation! (", event.membership, ")")
         return
-    
 
+    # join the room the bot is invited to
     roomid=room.room_id
-    await client.join(roomid)                                                           #Join to every room the bot is invited
+    await client.join(roomid)
     print("Raum '" + room.display_name + "' beigetreten")
-    print(event)
+
+    # send a message about having joined
     await client.room_send(
         room_id=roomid,
         message_type="m.room.message",
@@ -72,7 +99,6 @@ async def call_on_invites(room, event):
         username=str(member.user_id).split("@")[1].split(":")[0]
         print(username)
         
-    
     #await client.room_invite(roomid, "@username:url")                      #Add 'musterfrau' and leave the room
     #await client.room_leave(roomid)
     #await client.close()
@@ -86,7 +112,15 @@ async def main():
     client.add_event_callback(call_on_invites, InviteEvent)
     await client.sync_forever(30000)
 
+async def logout():
+    await client.close()
+
+
 loop = asyncio.get_event_loop()
+
+    
+
+
 login_response = loop.run_until_complete(login())
 
 while True:
